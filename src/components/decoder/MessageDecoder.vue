5107504daaee3a29e46d276a228e859a107c9e17
Guistoff081 <55218750+Guistoff081@users.noreply.github.com>
2025-01-14 16:20:38 -0300

    first commit

---
+++ src/components/decoder/MessageDecoder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="decoder-input">
+    <AlienMessage />
+    <DecoderInput v-model="model" />
+  </div>
+</template>
+
+<script setup>
+import AlienMessage from './AlienMessage.vue';
+import DecoderInput from './DecoderInput.vue';
+
+const model = defineModel();
+</script>
+
+<style scoped>
+.decoder-input {
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
